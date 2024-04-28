4986047d0792241df5036719367ca9d3933b16a3
ZhangZJ0906 <cklgy88@gmail.com>
2024-04-28 20:07:51 +0800

    new common.js new function redirectToRoute

---
+++ public/js/common.js
@@ -0,0 +1,4 @@
+function redirectToRoute(route) {
+    window.location = route;
+}
+
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -26,7 +26,7 @@
       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
     </form>
     <div class="login">
-      <button  onclick="window.location='{{ route('login') }}'" >Login</button>
+    <button  onclick="redirectToRoute('login')" >Login</button>
         
 
     </div>
@@ -36,5 +36,6 @@
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script src="{{asset('js/common.js')}}"></script>
 </body>
 </html>
